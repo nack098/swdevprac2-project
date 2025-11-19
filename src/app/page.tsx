1d905792ad791ca4bdaad812f1992d38545132de
Pooh5821 <pooh5821ingkanunt@gmail.com>
2025-11-20 01:53:59 +0700

    fix(home) - add latest products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,39 @@
 "use client";
-import { Box, Carousel, Heading, IconButton, Image, Link, SimpleGrid, Span, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Carousel,
+  For,
+  Heading,
+  IconButton,
+  Image,
+  Link,
+  SimpleGrid,
+  Span,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { useRef } from "react";
 import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
-import NextLink from "next/link"
+import NextLink from "next/link";
 import { FaAngleRight } from "react-icons/fa";
-import log from "@/libs/utils/log";
+import { get } from "@/libs/apis/arttoys";
+import ProductCard from "@/components/ProductCard";
 
-const items = [
-  "/carousel1.webp",
-  "/carousel2.webp",
-  "/carousel3.webp",
-]
+const items = ["/carousel1.webp", "/carousel2.webp", "/carousel3.webp"];
 
-export default function Home() {
+async function getSortedProducts() {
+  const res = await get();
+  const products = res.data;
+  const sortedProducts = products.sort(
+    (a: any, b: any) =>
+      new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+  );
+  return sortedProducts;
+}
+
+export default async function Home() {
   const playButtonRef = useRef<HTMLButtonElement>(null);
+  const sortedFive = await getSortedProducts().then((data) => data.slice(0, 5));
   return (
     <>
       <Carousel.Root
@@ -22,9 +42,11 @@ export default function Home() {
         slidesPerMove={1}
         autoplay={{ delay: 4000 }}
         loop
-        onDragStatusChange={(details) => !details.isDragging ? playButtonRef.current?.click() : null}
+        onDragStatusChange={(details) =>
+          !details.isDragging ? playButtonRef.current?.click() : null
+        }
       >
-        <Carousel.ItemGroup >
+        <Carousel.ItemGroup>
           {items.map((link, index) => (
             <Carousel.Item key={index} index={index}>
               <Image src={link} height="35rem" width="full" objectFit="cover" />
@@ -48,20 +70,48 @@ export default function Home() {
           </Carousel.NextTrigger>
 
           <Carousel.AutoplayTrigger asChild>
-            <IconButton aria-label="Toggle autoplay" size="sm" display="none" ref={playButtonRef} />
+            <IconButton
+              aria-label="Toggle autoplay"
+              size="sm"
+              display="none"
+              ref={playButtonRef}
+            />
           </Carousel.AutoplayTrigger>
         </Carousel.Control>
       </Carousel.Root>
-      <Heading as="h1" justifyContent="center" display="flex" width="full" fontWeight="bold" fontSize="2xl" textAlign="center" marginTop="2rem">
+      <Heading
+        as="h1"
+        justifyContent="center"
+        display="flex"
+        width="full"
+        fontWeight="bold"
+        fontSize="2xl"
+        textAlign="center"
+        marginTop="2rem"
+      >
         Welcome to Drunkman Art Toys Shop!!
       </Heading>
-      <Heading as="h4" justifyContent="center" display="flex" width="full" fontWeight="normal" fontSize="md" textAlign="center" marginBottom="3rem">
+      <Heading
+        as="h4"
+        justifyContent="center"
+        display="flex"
+        width="full"
+        fontWeight="normal"
+        fontSize="md"
+        textAlign="center"
+        marginBottom="3rem"
+      >
         We are so drunk that's why we open an art toys shop
       </Heading>
       <Box paddingX="7rem">
         <hr />
       </Box>
-      <Stack direction="row" justifyContent="space-between" paddingX="3rem" marginTop="2rem">
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        paddingX="3rem"
+        marginTop="2rem"
+      >
         <Heading as="h1" fontWeight="bold">
           Our New Products
         </Heading>
@@ -70,15 +120,24 @@ export default function Home() {
           href="/products"
           textDecorationColor="bg"
           transitionTimingFunction="ease-out"
-          transition="colors" transitionDuration="300ms"
+          transition="colors"
+          transitionDuration="300ms"
           _hover={{ textDecorationColor: "inherit" }}
         >
-          All Products<FaAngleRight />
+          All Products
+          <FaAngleRight />
         </Link>
       </Stack>
-      <SimpleGrid minChildWidth="20rem" gap="3rem" paddingX="5rem" marginY="2rem">
+      <SimpleGrid
+        minChildWidth="20rem"
+        gap="3rem"
+        paddingX="5rem"
+        marginY="2rem"
+      >
         {/* Show 5 newest products (also change this shit to for loop or I will kill you */}
-        {/* <ProductCard /> */}
+        {sortedFive.map((product: any) => (
+          <ProductCard data={product} />
+        ))}
       </SimpleGrid>
     </>
   );
