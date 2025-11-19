7db78173fe0a81f43b6cdca1535120ffa1b959a9
Pooh5821 <pooh5821ingkanunt@gmail.com>
2025-11-20 02:59:59 +0700

    fix(product) - connect api

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,36 +1,56 @@
-"use client"
-import { use } from "react"
+"use client";
+import { use } from "react";
 import { Box, Image, HStack, Heading, Text, Button } from "@chakra-ui/react";
+import { getById } from "@/libs/apis/arttoys";
 
 interface Props {
-  productId: string
+  productId: string;
 }
 
-const test: ProductDetail = {
-  _id: "abc",
-  sku: "abc",
-  name: "Abc",
-  description: "blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla",
-  arrivalDate: "09/12/2003",
-  availableQuota: 0,
-  posterPicture: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsuperdeep.org%2Fwp-content%2Fuploads%2F2021%2F03%2FKaws-Gone-Companion-Vinyl-Figure-Art-Toy-2.jpg&f=1&nofb=1&ipt=1a08d2aff1c2977443752862d546e21e0879cc0ec8f67af97f164ea377cd4ee9",
-  createdAt: "2025-11-19T10:54:29.654Z",
-  updatedAt: "2025-11-19T10:54:29.654Z"
-}
+// const test: ProductDetail = {
+//   _id: "abc",
+//   sku: "abc",
+//   name: "Abc",
+//   description: "blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla",
+//   arrivalDate: "09/12/2003",
+//   availableQuota: 0,
+//   posterPicture: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsuperdeep.org%2Fwp-content%2Fuploads%2F2021%2F03%2FKaws-Gone-Companion-Vinyl-Figure-Art-Toy-2.jpg&f=1&nofb=1&ipt=1a08d2aff1c2977443752862d546e21e0879cc0ec8f67af97f164ea377cd4ee9",
+//   createdAt: "2025-11-19T10:54:29.654Z",
+//   updatedAt: "2025-11-19T10:54:29.654Z"
+// }
 
-export default function ProductPage({ params }: { params: Promise<Props> }) {
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<Props>;
+}) {
   const { productId } = use(params);
-  const product = test
+  const res = await getById(productId);
+  const product = res.data;
   return (
-    <Box paddingX="2rem" marginX="10rem" paddingY="3rem" marginY="2rem" rounded="md"
+    <Box
+      paddingX="2rem"
+      marginX="10rem"
+      paddingY="3rem"
+      marginY="2rem"
+      rounded="md"
       boxShadow={{ _light: "0 0 0.1rem 0 black", _dark: "0 0 0.1rem 0 white" }}
     >
-      <HStack justifyContent="center" gap="12rem" align="start" textAlign="left">
-        <Image src={product.posterPicture} height="50rem" width="50rem" objectFit="cover" rounded="md" />
+      <HStack
+        justifyContent="center"
+        gap="12rem"
+        align="start"
+        textAlign="left"
+      >
+        <Image
+          src={product.posterPicture}
+          height="50rem"
+          width="50rem"
+          objectFit="cover"
+          rounded="md"
+        />
         <Box width="35rem" rounded="md">
-          <Text>
-            {product.sku}
-          </Text>
+          <Text>{product.sku}</Text>
           <Heading as="h1" fontSize="5xl" fontWeight="bold" lineHeight="3rem">
             {product.name}
           </Heading>
@@ -45,5 +65,5 @@ export default function ProductPage({ params }: { params: Promise<Props> }) {
         </Box>
       </HStack>
     </Box>
-  )
+  );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,22 +1,28 @@
-"use client"
+"use client";
+import ProductCard from "@/components/ProductCard";
+import { get } from "@/libs/apis/arttoys";
 import { Heading, SimpleGrid, Box, Link, Text, Image } from "@chakra-ui/react";
-import NextLink from "next/link"
 
-const items = [
-  "/carousel1.webp",
-  "/carousel2.webp",
-  "/carousel3.webp",
-]
+const items = ["/carousel1.webp", "/carousel2.webp", "/carousel3.webp"];
 
-export default function ProductPage() {
+export default async function ProductPage() {
+  const res = await get();
+  const products = res.data;
   return (
     <Box paddingX={{ base: "2rem", md: "10rem" }} paddingY="5rem">
       <Heading as="h1" fontWeight="bold" fontSize="3xl" marginBottom="2rem">
         All Products
       </Heading>
-      <SimpleGrid minChildWidth={{ md: "20rem" }} gap={{ base: "2rem", md: "5rem" }}>
-        {/*<ProductCard />*/}
+      <SimpleGrid
+        minChildWidth={{ md: "20rem" }}
+        gap={{ base: "2rem", md: "5rem" }}
+      >
+        {products.data.map((product: any) => (
+          <Link href={`/products/${product._id}`}>
+            <ProductCard data={product}></ProductCard>
+          </Link>
+        ))}
       </SimpleGrid>
     </Box>
-  )
+  );
 }
