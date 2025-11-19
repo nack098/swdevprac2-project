de40731bc140ad565e0136bce570cac78e2a060f
Pooh5821 <pooh5821ingkanunt@gmail.com>
2025-11-20 01:38:40 +0700

    fix(order) - add get orders

--- src/app/myorders/page.tsx
+++ src/app/myorders/page.tsx
@@ -1,60 +1,32 @@
 "use client";
 
-import { Box, Heading, Text, SimpleGrid, Button } from "@chakra-ui/react";
-import { useRouter } from "next/navigation";
+import { get } from "@/libs/apis/orders";
+import { SimpleGrid } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
+import OrderCard from "@/components/OrderCard";
 
-interface Props {
-  toyName: string;
-  amount: number;
-  member?: string;
-}
+export default async function MyOrdersPage() {
+  const { data: session } = useSession();
+  const token = session?.user?.token;
 
-function OrderCard(props: Props) {
-  const router = useRouter();
-  const handleEdit = () => {
-    router.push("/order/edit");
-  };
-  return (
-    <Box
-      bg="purple.200"
-      borderRadius="md"
-      shadow="lg"
-      padding="1.5rem"
-      margin="1rem"
-      width="44rem"
-      height="12rem"
-      position="relative"
-    >
-      <Heading as="h3" size="xl" fontWeight="bold" color="black" mb="0.5rem">
-        {props.toyName}
-      </Heading>
-      <Text fontSize="md" color="black">
-        Amount ordered: {props.amount}
-      </Text>
-      {props.member && (
-        <Text fontSize="md" color="black">
-          Ordered by: {props.member}
-        </Text>
-      )}
-      <Button
-        colorPalette="gray"
-        bottom="2rem"
-        position="absolute"
-        onClick={handleEdit}
-      >
-        Edit
-      </Button>
-    </Box>
-  );
-}
-
-export default function MyOrdersPage() {
+  if (!token) {
+    return null;
+  }
+  const orders = await get(token);
   return (
     <SimpleGrid minChildWidth="40rem" columns={2} gap="2rem">
-      <OrderCard toyName="Toy1" amount={2} member="Alice" />
+      {/* <OrderCard toyName="Toy1" amount={2} member="Alice" />
       <OrderCard toyName="Toy2" amount={1} member="Bob" />
       <OrderCard toyName="Toy3" amount={3} />
-      <OrderCard toyName="Toy4" amount={1} />
+      <OrderCard toyName="Toy4" amount={1} /> */}
+      {orders.data.map((order: any) => (
+        <OrderCard
+          key={order._id}
+          toyName={order.artToy}
+          amount={order.orderAmount}
+          member={order.user}
+        />
+      ))}
     </SimpleGrid>
   );
 }
---
+++ src/components/OrderCard.tsx
@@ -0,0 +1,47 @@
+import { Box, Heading, Button, Text } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  toyName: string;
+  amount: number;
+  member?: string;
+}
+
+export default function OrderCard(props: Props) {
+  const router = useRouter();
+  const handleEdit = () => {
+    router.push("/order/edit");
+  };
+  return (
+    <Box
+      bg="purple.200"
+      borderRadius="md"
+      shadow="lg"
+      padding="1.5rem"
+      margin="1rem"
+      width="44rem"
+      height="12rem"
+      position="relative"
+    >
+      <Heading as="h3" size="xl" fontWeight="bold" color="black" mb="0.5rem">
+        {props.toyName}
+      </Heading>
+      <Text fontSize="md" color="black">
+        Amount ordered: {props.amount}
+      </Text>
+      {props.member && (
+        <Text fontSize="md" color="black">
+          Ordered by: {props.member}
+        </Text>
+      )}
+      <Button
+        colorPalette="gray"
+        bottom="2rem"
+        position="absolute"
+        onClick={handleEdit}
+      >
+        Edit
+      </Button>
+    </Box>
+  );
+}
