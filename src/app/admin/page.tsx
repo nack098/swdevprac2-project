40a0f6e0373e606227d168683f16c327488691fb
Pooh5821 <pooh5821ingkanunt@gmail.com>
2025-11-20 11:48:14 +0700

    fix(order) - add missing link

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,10 @@
-"use client"
-import { use, useEffect, useState } from "react"
+"use client";
+import { use, useEffect, useState } from "react";
 import ProductCard from "@/components/ProductCard";
 import { useAppSelector } from "@/redux/store";
 import { Box, Heading, HStack, Link, SimpleGrid } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
-import NextLink from "next/link"
+import NextLink from "next/link";
 import { get } from "@/libs/apis/orders";
 import OrderCard from "@/components/OrderCard";
 
@@ -23,19 +23,28 @@ export default function AdminPage() {
 
   useEffect(() => {
     if (data?.user.token) {
-      const promise = get(data.user.token)
+      const promise = get(data.user.token);
 
-      promise.then(value => { changeOrders(value.data); console.log(value.data) })
+      promise.then((value) => {
+        changeOrders(value.data);
+        console.log(value.data);
+      });
 
-      return (() => { Promise.reject(promise) })
+      return () => {
+        Promise.reject(promise);
+      };
     }
-  }, [data])
+  }, [data]);
 
   console.log(products);
 
   const sortedFive = getSortedProducts(products).slice(0, 5);
   if (data?.user.role !== "admin") {
-    return <Heading as="h1" fontSize="3xl" fontWeight="bold">Unauthorized</Heading>
+    return (
+      <Heading as="h1" fontSize="3xl" fontWeight="bold">
+        Unauthorized
+      </Heading>
+    );
   }
 
   return (
@@ -46,9 +55,16 @@ export default function AdminPage() {
       <Box h="25rem">
         <HStack justifyContent="space-between">
           <Heading as="h1">Latest Product</Heading>
-          <Link as={NextLink} href="/admin/products">All Products</Link>
+          <Link as={NextLink} href="/admin/products">
+            All Products
+          </Link>
         </HStack>
-        <SimpleGrid minChildWidth="20rem" gap="3rem" paddingX="5rem" marginY="2rem">
+        <SimpleGrid
+          minChildWidth="20rem"
+          gap="3rem"
+          paddingX="5rem"
+          marginY="2rem"
+        >
           {/* Show 5 newest products (also change this shit to for loop or I will kill you */}
           {sortedFive.map((product: any) => (
             <ProductCard data={product} />
@@ -58,14 +74,31 @@ export default function AdminPage() {
       <Box h="10rem">
         <HStack justifyContent="space-between">
           <Heading as="h1">Latest Orders</Heading>
-          <Link as={NextLink} href="/admin/orders">All Orders</Link>
+          <Link as={NextLink} href="/admin/orders">
+            All Orders
+          </Link>
         </HStack>
-        <SimpleGrid minChildWidth="20rem" gap="3rem" paddingX="5rem" marginY="2rem">
+        <SimpleGrid
+          minChildWidth="20rem"
+          gap="3rem"
+          paddingX="5rem"
+          marginY="2rem"
+        >
           {/* Show 5 newest orders (also change this shit to for loop or I will kill you */}
           {/* <ProductCard /> */}
-          {orders ? getSortedProducts(orders.data).map((order: any) => <OrderCard toyName={order.artToy.name} amount={order.orderAmount} />) : <></>}
+          {orders ? (
+            getSortedProducts(orders.data).map((order: any) => (
+              <OrderCard
+                toyName={order.artToy.name}
+                amount={order.orderAmount}
+                link={`/admin/products/${order.artToy._id}`}
+              />
+            ))
+          ) : (
+            <></>
+          )}
         </SimpleGrid>
       </Box>
     </Box>
-  )
+  );
 }
